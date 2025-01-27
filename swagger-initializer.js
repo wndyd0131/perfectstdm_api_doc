d011e7b2b540876ace86444749d57e4c93e7b312
wndyd <wndyd0131@naver.com>
2025-01-27 16:32:05 -0600

    updated

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/SkkuKapstone/2024_Kapstone/refs/heads/wndyd/backend/swagger/swagger.json",
+    url: "https://raw.githubusercontent.com/wndyd0131/perfectstdm_api_doc/refs/heads/main/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
